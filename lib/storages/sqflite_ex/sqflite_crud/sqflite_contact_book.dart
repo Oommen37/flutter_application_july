b076fe06ce2cae7b80865bed0488b14b3f5672a6
Oommen37 <nohankurian@gmail.com>
2023-10-27 11:06:01 +0530

    commit

--- lib/storages/sqflite_ex/sqflite_crud/sqflite_contact_book.dart
+++ lib/storages/sqflite_ex/sqflite_crud/sqflite_contact_book.dart
@@ -42,19 +42,25 @@ class _Contact_BookState extends State<Contact_Book> {
                             Colors.primaries[index % Colors.primaries.length],
                       ),
                     ),
-                    title: Text(contacts[index]["cname"]),
-                    subtitle: Text(contacts[index]["cnumber"]),
+                    title: Text(contacts[index]['cname']),
+                    subtitle: Text(contacts[index]['cnumber']),
                     trailing: Wrap(
                       children: [
-                        IconButton(onPressed: () {}, icon: Icon(Icons.edit)),
-                        IconButton(onPressed: () {}, icon: Icon(Icons.delete))
+                        IconButton(
+                          onPressed: () => showSheet(contacts[index]["id"]),
+                          icon: Icon(Icons.edit),
+                        ),
+                        IconButton(
+                            onPressed: () =>
+                                deleteContact(contacts[index]["id"]),
+                            icon: Icon(Icons.delete))
                       ],
                     ),
                   ),
                 );
               }),
       floatingActionButton: FloatingActionButton(
-        onPressed: () => showsheet(null),
+        onPressed: () => showSheet(null),
         child: Icon(Icons.person_add),
       ),
     );
@@ -62,9 +68,14 @@ class _Contact_BookState extends State<Contact_Book> {
 
   final name_controller = TextEditingController();
   final num_controller = TextEditingController();
-  showsheet(int? id) {
+  void showSheet(int? id) {
+    if (id != null) {
+      final existingcontact =
+          contacts.firstWhere((element) => element["id"] == id);
+      name_controller.text = existingcontact["cname"];
+      num_controller.text = existingcontact["cnumber"];
+    }
     showModalBottomSheet(
-        isScrollControlled: true,
         context: context,
         builder: (context) {
           return Container(
@@ -74,49 +85,68 @@ class _Contact_BookState extends State<Contact_Book> {
                 right: 15,
                 bottom: MediaQuery.of(context).viewInsets.bottom + 120),
             child: Column(
+              mainAxisSize: MainAxisSize.min,
               children: [
                 TextField(
                   controller: name_controller,
-                  decoration: const InputDecoration(
-                      border: OutlineInputBorder(), hintText: "Name"),
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(), hintText: 'Name'),
                 ),
                 TextField(
                   controller: num_controller,
-                  decoration: const InputDecoration(
-                      border: OutlineInputBorder(), hintText: "Number"),
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(), hintText: 'number'),
                 ),
                 ElevatedButton(
                     onPressed: () {
                       if (id == null) {
-                        createContact(
+                        createcontact(
                             name_controller.text, num_controller.text);
                       }
                       if (id != null) {
-                        //updateContact();
+                        updateContact(id);
                       }
-                      name_controller.text = "";
-                      num_controller.text = "";
+                      name_controller.text = '';
+                      num_controller.text = '';
+                      Navigator.of(context).pop();
                     },
                     child:
-                        Text(id == null ? "Create Contact" : "Update Contact"))
+                        Text(id == null ? 'create contact' : 'update contact'))
               ],
             ),
           );
         });
   }
 
-  Future<void> createContact(String name, String number) async {
+  Future<void> createcontact(String name, String number) async {
     await SQL_Functions.addnewContact(name, number);
   }
 
   @override
   void initState() {
-    readContact_and_refresh_Ui() async {
-      final mycontacts = await SQL_Functions.readContacts();
-      setState(() {
-        contacts = mycontacts;
-        isLoading = false;
-      });
-    }
+    readContact_and_refresh_ui();
+    super.initState();
+  }
+
+  Future<void> readContact_and_refresh_ui() async {
+    final mycontacts = await SQL_Functions.readContact();
+    setState(() {
+      contacts = mycontacts;
+      isLoading = false;
+    });
+  }
+
+  Future<void> updateContact(int id) async {
+    await SQL_Functions.updateContactt(
+        id, name_controller.text, num_controller.text);
+    readContact_and_refresh_ui();
+  }
+
+  Future<void> deleteContact(int id) async {
+    await SQL_Functions.removeContact(id);
+    readContact_and_refresh_ui();
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      content: Text("Successfully Deleted"),
+    ));
   }
 }
--- lib/storages/sqflite_ex/sqflite_crud/sql_functions.dart
+++ lib/storages/sqflite_ex/sqflite_crud/sql_functions.dart
@@ -12,23 +12,37 @@ class SQL_Functions {
 
   //create Table
   static Future<void> createTable(sql.Database db) async {
-    await db.execute("""CREATE TABLE user(
-       id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-       cname TEXT,
-       cnumber TEXT,
-      )""");
+    await db.execute(
+        'CREATE TABLE contact (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, cname TEXT,  cnumber TEXT)');
   }
 
   static Future<int> addnewContact(String name, String number) async {
     final db = await SQL_Functions.OpenDb();
-    final data = {"cname": name, "cnumber": number};
-    final id = await db.insert("contact", data,
+    final data = {'cname': name, 'cnumber': number};
+    final id = await db.insert('contact', data,
         conflictAlgorithm: ConflictAlgorithm.replace);
     return id;
   }
 
-  static Future<List<Map<String, dynamic>>> readContacts() async {
+  static Future<List<Map<String, dynamic>>> readContact() async {
     final db = await SQL_Functions.OpenDb();
-    return db.query("contact", orderBy: "id");
+    return db.query('contact', orderBy: 'id');
+  }
+
+  static Future<int> updateContactt(int id, String name, String num) async {
+    final db = await SQL_Functions.OpenDb();
+    final updateddata = {"cname": name, "cnumber": num};
+    final updatedid =
+        db.update("contact", updateddata, where: "id=?", whereArgs: [id]);
+    return updatedid;
+  }
+
+  static Future<void> removeContact(int id) async {
+    final db = await SQL_Functions.OpenDb();
+    try {
+      await db.delete("contact", where: "id=?", whereArgs: [id]);
+    } catch (e) {
+      print("Something went wrong $e");
+    }
   }
 }
